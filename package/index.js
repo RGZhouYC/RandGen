eb554cc946e2c8139f12310b3242bf767161fb6d
RGZhouYC <RGZhouYC@users.noreply.github.com>
2023-02-24 18:43:51 -0500

    matrix generation added

--- package/index.js
+++ package/index.js
@@ -10,6 +10,20 @@ function GenerateRandomArray(length, min, max, decimal){
     return output
 }
 
+function GenerateRandomMatrix(rows, cols, min, max, decimal){
+    var outputmatrix = []
+    for (let i = 0; i < rows; i++){
+        var row = []
+        for (let j = 0; j < cols; j++){
+            let randNum = Math.random() * (max - min) + min
+            randNum = randNum.toFixed(decimal)
+            row.push(randNum)
+        }
+        outputmatrix.push(row)
+    }
+    return outputmatrix
+}
+
 // will be made to be a private utility later
 function exportData(source, exportType, exportPath) {
     switch(exportType){
@@ -58,15 +72,30 @@ function GenerateRandomCol(length, min, max, decimal, exportType, exportPath){
     return output
 }
 
-function GenerateRandomMetrix(rows, cols, min, max, decimal, exportType, exportPath){
-
+function ExportRandomMatrix(rows, cols, min, max, decimal, exportType, exportPath) {
+    var output = GenerateRandomMatrix(rows, cols, min, max, decimal)
+    var outputstring = ''
+    var des = '' // final export destination 
+    for (let i = 0; i < rows; i++) {
+        var rowstring = ''
+        for (let j = 0; j < cols; j++) {
+            var item = `${output[i][j]} `
+            rowstring = rowstring + item
+        }
+        outputstring = outputstring + '\n' + rowstring
+    }
+    exportData(outputstring, exportType, exportPath)
+    return output
 }
 
 function GenerateByPattern(){
 
 }
 
+module.exports = GenerateRandomArray, GenerateRandomRow, GenerateRandomCol, GenerateRandomMatrix, GenerateByPattern,
+ExportRandomMatrix
+
 //testing commands
 //GenerateRandomCol(7, 56, 80, 2, 'console', '.')
 //console.log(GenerateRandomArray(7, 56, 80, 2));
-module.exports = GenerateRandomArray, GenerateRandomRow, GenerateRandomCol, GenerateRandomMetrix, GenerateByPattern
+//ExportRandomMatrix(7, 3, 56, 80, 2, 'txt', '.')
