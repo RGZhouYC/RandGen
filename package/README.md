027c37c98611bea00724d282f5b2639756c83f93
RGZhouYC <RGZhouYC@users.noreply.github.com>
2023-02-24 17:39:00 -0500

    just a clean up

--- package/README.md
+++ package/README.md
@@ -1,4 +1,4 @@
-# randgenMatrix-npm-pakcage
+# randgenMatrix-npm-pakcage (under development)
 
 ## randgenMatrix
 Generate array or matrix of random number with specified range, size, decimal, and export to txt or csv file. 
@@ -7,5 +7,10 @@ Generate array or matrix of random number with specified range, size, decimal, a
     > npm install randgenMatrix
 
 ## Usage
+### GenerateRandomArray
 Generate an array of random number. min and max specifies the range, length specifies the number of elements, decimal specifies the number of digits to be saved after decimal point.
 > GenerateRandomArray(length, min, max, decimal)
+Example: 
+> GenerateRandomCol(7, 56, 80, 2)
+    returns: 
+> ['79.11', '67.02', '65.51', '70.45', '75.17', '76.71', '62.92']
--- package/index.js
+++ package/index.js
@@ -1,3 +1,5 @@
+const fs = require('fs');
+
 function GenerateRandomArray(length, min, max, decimal){
     var output = []
     for (let i = 0; i < length; i++) {
@@ -8,14 +10,52 @@ function GenerateRandomArray(length, min, max, decimal){
     return output
 }
 
+// will be made to be a private utility later
+function exportData(source, exportType, exportPath) {
+    switch(exportType){
+        case 'console':
+            console.log(source)
+        case 'txt':
+            des = exportPath + '/output.txt'
+            fs.writeFile(des, source, err => {
+                if (err) {
+                  console.error(err);
+                }
+            })
+            break
+        case 'cvs':
+            des = exportPath + '/output.cvs'
+            fs.writeFile(des, source, err => {
+                if (err) {
+                  console.error(err);
+                }
+            })
+            break
+    }
+}
+
 function GenerateRandomRow(length, min, max, decimal, exportType, exportPath){
     var output = GenerateRandomArray(length, min, max, decimal)
-    console.log(output)
+    var outputstring = ''
+    var des = '' // final export destination 
+    for (let i = 0; i < length; i++){
+        var item = `${output[i]} `
+        outputstring = outputstring + item
+    } 
+    exportData(outputstring, exportType, exportPath)
+    return output
 }
 
 function GenerateRandomCol(length, min, max, decimal, exportType, exportPath){
     var output = GenerateRandomArray(length, min, max, decimal)
-    console.log(output)
+    var outputstring = ''
+    var des = '' // final export destination 
+    for (let i = 0; i < length; i++){
+        var item = `${output[i]}\n`
+        outputstring = outputstring + item
+    } 
+    exportData(outputstring, exportType, exportPath)
+    return output
 }
 
 function GenerateRandomMetrix(rows, cols, min, max, decimal, exportType, exportPath){
@@ -26,5 +66,7 @@ function GenerateByPattern(){
 
 }
 
-GenerateRandomCol(7, 56, 80, 2, 4, 5)
+//testing commands
+//GenerateRandomCol(7, 56, 80, 2, 'console', '.')
+//console.log(GenerateRandomArray(7, 56, 80, 2));
 module.exports = GenerateRandomArray, GenerateRandomRow, GenerateRandomCol, GenerateRandomMetrix, GenerateByPattern
